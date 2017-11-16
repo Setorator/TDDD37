36fbb0a9d3b61288da5c05898ea8bde048b1201f
Janos <janos.r.dani@gmail.com>
2017-11-16 14:29:59 +0100

    Completed task 3

--- lab2/lab2_manager.sql
+++ lab2/lab2_manager.sql
@@ -3,14 +3,35 @@
 
 
 create table jbmanager
-       (id int,
-       bonus int,
+       (id integer,
+       bonus integer default null,
        
-       constraint pk-manager primary key(id),
+       constraint pk_manager primary key(id),
        
        constraint fk_employee foreign key(id) references jbemployee(id));
 
-alter table jbemployee drop constraint fk_dept_mgr;
-alter table jbemployee add constraint fk_dept_mgr foreign key (manager) references jbmanager(id) on delete set null;
+-- Insert manager id from jbemployee, excluding NULL
+insert into jbmanager
+select manager, null
+from jbemployee
+where manager in (select id from jbemployee)
+group by jbemployee.manager;
 
-alter table jbemployee add constraint fk_dept_mgr foreign key (manager) references jbmanager(id) on delete set null;
+
+-- Insert manager id from jbdept who is not already part of jbmanager, excluding NULL
+insert into jbmanager
+select manager, null
+from jbdept
+where manager not in (select id from jbmanager)
+group by jbdept.manager;
+
+
+-- Rearange foreign keys
+alter table jbdept drop foreign key fk_dept_mgr;
+alter table jbdept add constraint fk_dept_mgr foreign key (manager) references jbmanager(id) on delete set null;
+
+alter table jbemployee drop foreign key fk_emp_mgr;
+alter table jbemployee add constraint fk_emp_mgr foreign key (manager) references jbmanager(id) on delete set null;
+
+-- select * from jbmanager;
+
