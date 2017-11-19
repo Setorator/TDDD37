ecfb1cf240b4cd5f9f52bddb1f1944ab75fa69c2
Kim Larsson <kimla207@li17-5.ida.liu.se>
2017-11-19 15:26:03 +0100

    Finished Lab 2 We have completed lab 2 and handed in the answers to the lab assisstant.

--- lab2/task3.sql
+++ lab2/task3.sql
@@ -41,5 +41,56 @@ alter table jbdept add constraint fk_dept_mgr foreign key (manager) references j
 alter table jbemployee drop foreign key fk_emp_mgr;
 alter table jbemployee add constraint fk_emp_mgr foreign key (manager) references jbmanager(id) on delete set null;
 
--- select * from jbmanager;
+ select * from jbmanager;
+
+
+
+/*
+###########################
+# Results of the commands #
+###########################
+
+
+mysql> source task3.sql;
+Query OK, 0 rows affected (0.01 sec)
+
+Query OK, 8 rows affected (0.01 sec)
+Records: 8  Duplicates: 0  Warnings: 0
+
+Query OK, 4 rows affected (0.00 sec)
+Records: 4  Duplicates: 0  Warnings: 0
+
+Query OK, 19 rows affected (0.03 sec)
+Records: 19  Duplicates: 0  Warnings: 0
+
+Query OK, 19 rows affected (0.03 sec)
+Records: 19  Duplicates: 0  Warnings: 0
+
+Query OK, 25 rows affected (0.03 sec)
+Records: 25  Duplicates: 0  Warnings: 0
+
+Query OK, 25 rows affected (0.05 sec)
+Records: 25  Duplicates: 0  Warnings: 0
+
++-----+-------+
+| id  | bonus |
++-----+-------+
+|  10 |     0 |
+|  13 |     0 |
+|  26 |     0 |
+|  32 |     0 |
+|  33 |     0 |
+|  35 |     0 |
+|  37 |     0 |
+|  55 |     0 |
+|  98 |     0 |
+| 129 |     0 |
+| 157 |     0 |
+| 199 |     0 |
++-----+-------+
+12 rows in set (0.00 sec)
+
+
+
+*/
 
--- lab2/task4.sql
+++ lab2/task4.sql
@@ -1,6 +1,40 @@
 
 
+-- Update bonus for department managers to the earlier value plus 10 000
 update jbmanager
 set bonus = bonus + 10000
 where id in (select manager from jbdept);
+
+select * from jbmanager;
+
+/*
+###########################
+# Results of the commands #
+###########################
+
+mysql> source task4.sql;
+Query OK, 11 rows affected (0.00 sec)
+Rows matched: 11  Changed: 11  Warnings: 0
+
++-----+-------+
+| id  | bonus |
++-----+-------+
+|  10 | 10000 |
+|  13 | 10000 |
+|  26 | 10000 |
+|  32 | 10000 |
+|  33 | 10000 |
+|  35 | 10000 |
+|  37 | 10000 |
+|  55 | 10000 |
+|  98 | 10000 |
+| 129 | 10000 |
+| 157 | 10000 |
+| 199 |     0 |
++-----+-------+
+12 rows in set (0.00 sec)
+
+
+
+*/
 
