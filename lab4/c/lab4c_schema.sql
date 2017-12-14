94629d244307491f26ac330534a0be7fa83f6f7a
Kim Larsson <kimla207@li17-2.ida.liu.se>
2017-12-14 18:38:06 +0100

    Continued with lab 4 We have made it as far as to completion of task 4, we think, of lab 4. We also upload the test for checking the solution in task 3.

--- lab4/c/lab4c_schema.sql
+++ lab4/c/lab4c_schema.sql
@@ -3,20 +3,20 @@
 -- #############################################################
 
 -- Drop constraints
-alter table route drop foreign key fk_arrives_to;
-alter table route drop foreign key fk_departs_from;
-alter table weekday drop foreign key fk_weekday_year;
+alter table booked drop foreign key fk_credit_card;
+alter table booked drop foreign key fk_booked_res;
+alter table booked_pass drop foreign key fk_res_nr;
+alter table booked_pass drop foreign key fk_passenger;
+alter table contact drop foreign key fk_contact_pass;
+alter table reservation drop foreign key fk_flight;
+alter table reservation drop foreign key fk_resp;
+alter table flight drop foreign key fk_flight_schedule;
 alter table weekly_schedule drop foreign key fk_weekly_route;
 alter table weekly_schedule drop foreign key fk_day;
 alter table weekly_schedule drop foreign key fk_schedule_year;
-alter table contact drop foreign key fk_contact_pass;
-alter table flight drop foreign key fk_flight_schedule;
-alter table reservation drop foreign key fk_responsible;
-alter table reservation drop foreign key fk_flight;
-alter table reserved_pass drop foreign key fk_passenger;
-alter table reserved_pass drop foreign key fk_res_nr;
-alter table booked drop foreign key fk_booked_res;
-alter table booked drop foreign key fk_credit_card;
+alter table weekday drop foreign key fk_weekday_year;
+alter table route drop foreign key fk_arrives_to;
+alter table route drop foreign key fk_departs_from;
 
 -- Drop tables
 drop table airport;
@@ -29,9 +29,8 @@ drop table contact;
 drop table flight;
 drop table reservation;
 drop table credit_card;
-drop table reserved_pass;
+drop table booked_pass;
 drop table booked;
-
 
 -- #############################################################
 -- ################## Create tables here #######################
@@ -43,14 +42,14 @@ create table airport(
        name varchar(30) not null,
        country varchar(30) not null,
 
-       constraint pk_airport primary key(airport_id));
+       constraint pk_airport primary key(airport_code));
 
 -- Create table route 
 create table route(
-       route_id varchar(10) not null,
+       route_id varchar(12) not null,
        year integer not null,
-       arr_to integer not null,
-       dep_from integer not null,
+       arr_to varchar(3) not null,
+       dep_from varchar(3) not null,
        route_price double not null,
       
        constraint pk_route primary key(route_id));
@@ -68,8 +67,7 @@ create table weekday(
        year integer not null,
        profit double not null,
 
-       constraint pk_weekday primary key(day),
-       constraint pk_weekday primary key(year));
+       constraint pk_weekday primary key(day, year));
 
 -- Create table passenger 
 create table passenger(
@@ -80,8 +78,8 @@ create table passenger(
 
 -- Create table weekly_schedule 
 create table weekly_schedule(
-       schedule_id integer not null,
-       route integer not null,
+       schedule_id integer not null auto_increment,
+       route varchar(12) not null,
        dep_time timestamp not null,
        day varchar(10) not null,
        year integer not null,
@@ -98,7 +96,7 @@ create table contact(
 
 -- Create table flight 
 create table flight(
-       flight_id integer not null,
+       flight_id integer not null auto_increment,
        schedule integer not null,
        week integer not null,  
 
@@ -106,7 +104,7 @@ create table flight(
 
 -- Create table reservation 
 create table reservation(
-       res_number integer not null,
+       res_number integer not null auto_increment,
        contact integer not null,
        flight integer not null,
        nr_of_pass integer not null,
@@ -120,13 +118,13 @@ create table credit_card(
 
        constraint pk_credit_card primary key(card_nr));
 
--- Create table reserved_pass 
-create table reserved_pass(
+-- Create table booked_pass 
+create table booked_pass(
        pass_id integer not null,
        reservation_nr integer not null,
        ticket_nr integer not null,
 
-       constraint pk_reserved_pass primary key(pass_id));
+       constraint pk_booked_pass primary key(pass_id));
 
 -- Create table booked 
 create table booked(
@@ -141,54 +139,22 @@ create table booked(
 -- #############################################################
 
 
-alter table route add constraint fk_arrives_to foreign key (arr_to) references airport(airport_id);
-alter table route add constraint fk_departs_from foreign key (dep_from) references airport(airport_id);
+alter table route add constraint fk_arrives_to foreign key (arr_to) references airport(airport_code);
+alter table route add constraint fk_departs_from foreign key (dep_from) references airport(airport_code);
 alter table route add constraint fk_route_year foreign key (year) references year(year);
 alter table weekday add constraint fk_weekday_year foreign key (year) references year(year);
 alter table weekly_schedule add constraint fk_weekly_route foreign key (route) references route(route_id);
 alter table weekly_schedule add constraint fk_day foreign key (day) references weekday(day);
 alter table weekly_schedule add constraint fk_schedule_year foreign key (year) references weekday(year);
-alter table contact add constraint fk_contact_pass foreign key (contact_id) references passenger(pass_id);
 alter table flight add constraint fk_flight_schedule foreign key (schedule) references weekly_schedule(schedule_id);
-alter table reservation add constraint fk_responsible foreign key (contact) references contact(contact_id);
+alter table reservation add constraint fk_resp foreign key (contact) references contact(contact_id);
 alter table reservation add constraint fk_flight foreign key (flight) references flight(flight_id);
-alter table reserved_pass add constraint fk_passenger foreign key (pass_id) references passenger(pass_id);
-alter table reserved_pass add constraint fk_res_nr foreign key (reservation_nr) references reservation(res_number);
+alter table contact add constraint fk_contact_pass foreign key (contact_id) references passenger(pass_id);
+alter table booked_pass add constraint fk_passenger foreign key (pass_id) references passenger(pass_id);
+alter table booked_pass add constraint fk_res_nr foreign key (reservation_nr) references reservation(res_number);
 alter table booked add constraint fk_booked_res foreign key (reservation) references reservation(res_number);
 alter table booked add constraint fk_credit_card foreign key (card) references credit_card(card_nr);
 
 
--- #############################################################
--- ################## Set up procedures ########################
--- #############################################################
-
-delimiter //
-
-create procedure addYear(in year integer, in factor double)
-begin
-
-end//
-
-create procedure addDay(in year integer, in day varchar(10), in factor double)
-begin
-
-end//
-
-create procedure addDestination(in airport_id integer, in city_id integer)
-begin
-
-end//
-
-create procedure addYear(in year integer, in factor double)
-begin
-
-end//
-
-create procedure addYear(in year integer, in factor double)
-begin
-
-end//
 
 
-
-delimiter ;
---
+++ lab4/c/procedures.sql
@@ -0,0 +1,131 @@
+-- #############################################################
+-- ################## Set up procedures ########################
+-- #############################################################
+
+drop procedure if exists addYear;
+drop procedure if exists addDay;
+drop procedure if exists addDestination;
+drop procedure if exists addRoute;
+drop procedure if exists addFlight;
+
+drop function if exists calculateFreeSeats;
+drop function if exists calculatePrice;
+
+
+
+delimiter //
+
+create procedure addYear(in year integer, in factor double)
+begin
+	insert into year (year,profit) values (year, factor);
+end//
+
+create procedure addDay(in year integer, in day varchar(10), in factor double)
+begin
+	insert into weekday (day, year, profit) values (day, year, factor);
+end//
+
+create procedure addDestination(in airport_code varchar(3), in name varchar(30), in country varchar(30))
+begin
+	insert into airport (airport_code, name, country) values (airport_code, name, country);
+end//
+
+create procedure addRoute(in departure_airport_code varchar(3), in arrival_airport_code varchar(3), in year integer, in route_price double)
+begin
+	declare route_id varchar(12) default concat(departure_airport_code,'-',arrival_airport_code,'-',year);
+	insert into route (route_id, arr_to, dep_from, year, route_price) 
+	values (route_id, arrival_airport_code, departure_airport_code, year, route_price);
+end//
+
+
+-- This is, despite the name, the addWeeklySchedule procedure.
+create procedure addFlight(in departure_airport_code varchar(3), in arrival_airport_code varchar(3), in year integer, in day varchar(10), in departure_time time)
+begin
+	declare w integer default 1;
+	declare r_id varchar(12);
+	declare s_id integer;
+
+	select route_id into r_id from route 
+       	where route.arr_to = arrival_airport_code and route.dep_from = departure_airport_code and route.year = year;
+
+	insert into weekly_schedule (route, dep_time, day, year) 
+	values (r_id, departure_time, day, year);	
+
+	
+	select schedule_id into s_id from weekly_schedule order by weekly_schedule.schedule_id desc limit 1;
+	
+	while w < 53 do
+	      insert into flight (schedule, week) values (s_id, w);
+	      set w = w + 1;
+	end while;
+end//
+
+-- #############################################################
+-- #################### Helpfunctions ##########################
+-- #############################################################
+
+create function calculateFreeSeats(flightnumber integer)
+returns integer
+begin
+	declare seats integer default 40;
+	declare occupied_seats integer default 0;
+	declare free_seats integer default 0;
+
+	select count(*) into occupied_seats from booked_pass 
+	where reservation_nr in (select res_number from reservation where flight = flightnumber);
+		
+	set free_seats = seats - occupied_seats;
+	return free_seats;
+end //
+
+create function calculatePrice(flightnumber integer)
+returns double
+begin
+	declare total_price double;
+	declare route_factor double;
+	declare weekday_factor double;
+	declare profit_factor double;
+	declare booked_passengers double;
+
+	set booked_passengers = 40 - calculateFreeSeats(flightnumber);
+	
+	select route_price into route_factor 
+	from route 
+	where route_id in(
+	      select route 
+	      from weekly_schedule 
+	      where schedule_id in(
+	      	    select schedule 
+		    from flight 
+		    where flight_id = flightnumber));
+
+	select profit into weekday_factor
+	from weekday
+	where day in(
+	      select day
+	      from weekly_schedule
+	      where schedule_id in(
+	      	    select schedule 
+		    from flight 
+		    where flight_id = flightnumber));
+	
+	select profit into profit_factor
+	from year	
+	where year in(
+	      select year
+	      from weekly_schedule
+	      where schedule_id in(
+	      	    select schedule 
+		    from flight 
+		    where flight_id = flightnumber));
+
+	set total_price = route_factor * weekday_factor * (booked_passengers + 1.0)* 0.025 * profit_factor;
+	
+	return total_price;
+end //
+
+
+
+delimiter ;
+
+
